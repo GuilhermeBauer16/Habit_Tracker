a64e20cc186f03f1e3a82242febd3b67cd7d717d
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2025-09-27 10:56:39 -0300

    first commit

---
+++ src/test/java/br/com/HabitTracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.HabitTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
